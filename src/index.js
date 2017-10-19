b317c421b0bc61de372032986553081efaaab51c
Alan Souza <alansouzati@gmail.com>
2017-10-19 16:10:48 -0700

    Added grommet-icons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<div>hello!!!</div>, document.getElementById('root'));
-registerServiceWorker();
+import { Add } from 'grommet-icons';
+
+ReactDOM.render(<div><Add />hello!!!</div>, document.getElementById('root'));
 
 document.body.classList.remove('loading');
